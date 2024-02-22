b420568b4fe262ece1ffcd00f6788c0bd680fd41
JangHyunAh <wkdgusdk20@gmail.com>
2024-02-22 18:54:19 +0900

    FIX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,49 +1,50 @@
 class User < ApplicationRecord
   has_many :question_books
+  has_many :questions
   has_many :tags
-    attr_accessor :remember_token, :activation_token, :reset_token
-    before_save   :downcase_email
-    before_create :create_activation_digest
-    validates :name,  presence: true, length: { maximum: 50 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
-                      format: { with: VALID_EMAIL_REGEX },
-                      uniqueness: true
-    has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
-    # 有効化用のメールを送信する
-    def send_activation_email
-      UserMailer.account_activation(self).deliver_now
-    end
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  # 有効化用のメールを送信する
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
 
-     # メールアドレスをすべて小文字にする
-     def downcase_email
-      self.email = email.downcase
-     end
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
 
-    # 有効化トークンとダイジェストを作成および代入する
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 
-    # 渡された文字列のハッシュ値を返す
-    def User.digest(string)
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-      BCrypt::Password.create(string, cost: cost)
-    end
-  
-    # ランダムなトークンを返す
-    def User.new_token
-      SecureRandom.urlsafe_base64
-    end
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-     # アカウントを有効にする
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # アカウントを有効にする
   def activate
-        update_columns(activated: （コードを書き込む）, activated_at: （コードを書き込む）)
+    update_columns(activated: （コードを書き込む）, activated_at: （コードを書き込む）)
   end
 
   # パスワード再設定の属性を設定する
@@ -68,23 +69,23 @@ class User < ApplicationRecord
   end
 
 
-    # 永続的セッションのためにユーザーをデータベースに記憶する
-    def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
-        remember_digest
-    end
+  # 永続的セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
 
-    # セッションハイジャック防止のためにセッショントークンを返す
-    # この記憶ダイジェストを再利用しているのは単に利便性のため
-    def session_token
-        remember_digest || remember
-    end
+  # セッションハイジャック防止のためにセッショントークンを返す
+  # この記憶ダイジェストを再利用しているのは単に利便性のため
+  def session_token
+    remember_digest || remember
+  end
 
-    # 渡されたトークンがダイジェストと一致したらtrueを返す
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 
 end
